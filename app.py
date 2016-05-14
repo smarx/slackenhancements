617d50adc091c92517d12907f1754443df7de07b
Steve Marx <smarx@smarx.com>
2016-05-14 15:57:08 -0700

    whatevs

--- app.py
+++ app.py
@@ -10,19 +10,41 @@ sc = SlackClient(token)
 
 user_id = sc.api_call('auth.test')['user_id']
 
+MAX_COUNT = 40
+
+important = None
+
 stuff_to_do = []
 def do_stuff():
 	global stuff_to_do
 	print stuff_to_do
 	for thing in stuff_to_do:
 		thing['count'] -= 1
-		if thing['action'] == 'blink':
+		text = thing['text']
+
+		needs_update = False
+
+		if 'marquee' in thing['actions']:
+			text = thing['text'] + ' '*5
+			how_many = (MAX_COUNT-thing['count']) % len(text)
+			text = '`' + text[how_many:] + text[:how_many] + '`'
+			if thing['count'] == 0:
+				text = thing['text']
+			needs_update = True
+		if 'blink' in thing['actions'] and thing['count'] % 2 == 1:
+			text = ' '
+			needs_update = True
+
+		if needs_update:
 			sc.api_call('chat.update',
 				token=token,
 				channel=thing['channel'],
-				ts=thing['timestamp'],
-				text=thing['count'] % 2 == 0 and thing['text'] or ' ')
+				ts=thing['ts'],
+				text=text)
+
 	stuff_to_do = filter(lambda thing: thing['count'] > 0, stuff_to_do)
+	if important['count'] <= 0:
+		important = None
 
 if sc.rtm_connect():
 	while True:
@@ -30,16 +52,30 @@ if sc.rtm_connect():
 			if event['type'] == 'message' and event.get('user') == user_id:
 				channel = event['channel']
 				timestamp = event['ts']
-				blink_match = re.match(r'^&lt;blink&gt;(.*)&lt;/blink&gt;$', event['text'])
-				if blink_match:
-					text = blink_match.groups()[0]
-					stuff_to_do.append({
-						'action': 'blink',
-						'channel': channel,
-						'timestamp': timestamp,
-						'text': text,
-						'count': 40,
-					})
+
+				text = event['text']
+
+				actions_found = []
+				for action in ('blink', 'marquee', 'important'):
+					new_text = re.sub(r'&lt;{0}&gt;(.*)&lt;/{0}&gt;'.format(action),
+						r'\1', text)
+					if text != new_text:
+						actions_found.append(action)
+						text = new_text
+
+				thing = {
+					'actions': actions_found,
+					'channel': event['channel'],
+					'ts': event['ts'],
+					'text': match.groups()[0],
+					'count': MAX_COUNT,
+				}
+
+				if actions_found:
+					stuff_to_do.append(thing)
+				if 'important' in actions_found:
+					important = thing
+
 		time.sleep(0.25)
 		do_stuff()
 else:
