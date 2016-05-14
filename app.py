bf87f0a1d10f1dd4086a496b9cced176571dda5d
Steve Marx <smarx@smarx.com>
2016-05-14 14:59:38 -0700

    Find user's messages

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ token = os.environ['TOKEN']
 
 sc = SlackClient(token)
 
+user_id = sc.api_call('auth.test')['user_id']
+
 if sc.rtm_connect():
 	while True:
-		for thing in sc.rtm_read():
-			print thing
+		for event in sc.rtm_read():
+			if event['type'] == 'message' and event['user'] == user_id:
+				print 'I typed "%s" on channel "%s" with timestamp "%s"' % (
+					event['text'],
+					event['channel'],
+					event['ts'])
 		time.sleep(0.5)
 else:
 	print 'Connection failed.'
